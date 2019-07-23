db9fbd3cc4779552184d9a0cbe34f59473ac9049
Andrey Aksenov <armwres01@gmail.com>
2019-07-23 10:47:16 +0000

    My first commit

---
+++ check_api.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+w=` | grep pairId`
+sleep 2
+#second change git
+w=``
+if [ -z $w ];then
+	echo 1
+	echo `date`$w >> /.andaks/scripts/exc/error_contents.log
+#> /.andaks/scripts/exc/api.log
+else
+	echo 0
+# > /.andaks/scripts/exc/api.log
+fi
